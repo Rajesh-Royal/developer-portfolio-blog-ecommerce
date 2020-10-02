8578b4737e9b1d2bc1d9ffcb2b6b111607f4ca07
unknown <rajeshroyal896@gmail.com>
2020-10-02 08:45:03 +0530

    inject material ui theme first

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,14 @@ module.exports = {
     author: "@Rajesh-Royal",
   },
   plugins: [
-    "gatsby-plugin-material-ui",
+    {
+      resolve: "gatsby-plugin-material-ui",
+      options: {
+        stylesProvider: {
+          injectFirst: true,
+        },
+      },
+    },
     "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-source-filesystem",
